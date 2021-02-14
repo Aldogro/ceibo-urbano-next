dac4e8f7fb02255b09fb0cfa393938eb0f3d5655
Aldo Rojas <aldogrojas5@gmail.com>
2021-02-14 03:29:27 -0300

    added dropzone

--- modules/components/form/FormProduct.js
+++ modules/components/form/FormProduct.js
@@ -1,29 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
 import TextField from '@material-ui/core/TextField'
+import { DropzoneArea } from 'material-ui-dropzone'
 import { makeStyles } from '@material-ui/core/styles'
 
 const FormProduct = () => {
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const [price, setPrice] = useState('')
+  const [files, setFiles] = useState('')
+
+  const handleNameChange = ({ target }) => setName(target.value)
+  const handleDescriptionChange = ({ target }) => setDescription(target.value)
+  const handlePriceChange = ({ target }) => setPrice(target.value)
+
   const classes = useStyles()
 
   const onSubmit = (form) => {
     form.preventDefault()
-    console.log(form)
+    const formValue = {
+      name,
+      description,
+      price,
+      files,
+    }
+    console.log(formValue)
   }
 
   return (
     <React.Fragment>
         <form className={classes.root} onSubmit={onSubmit} noValidate autoComplete="off">
           <Grid container spacing={3} cols={2}>
+            <Grid item xs={6}>
+              <TextField className={classes.fullWidth} id="Name" label="Nombre" value={name} onChange={handleNameChange} />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField className={classes.fullWidth} id="Price" label="Precio" value={price} onChange={handlePriceChange} />
+            </Grid>
             <Grid item xs={12}>
-              <TextField className={classes.fullWidth} id="standard-basic" label="Standard" />
+              <TextField className={classes.fullWidth} id="Description" label="Descripción" value={description} onChange={handleDescriptionChange} />
             </Grid>
-            <Grid item xs={6}>
-              <TextField className={classes.fullWidth} id="standard-basic" label="Standard" />
-            </Grid>
-            <Grid item xs={6}>
-              <TextField className={classes.fullWidth} id="standard-basic" label="Standard" />
+            <Grid item xs={12}>
+              <DropzoneArea onChange={(files) => setFiles(files)} />
             </Grid>
           </Grid>
           <Grid item xs={12}>
