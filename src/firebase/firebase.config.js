62da833d318199542985a840626eee63082eaa23
Aldo Rojas <aldogrojas5@gmail.com>
2021-04-12 09:38:29 -0300

    env variables without NEXT

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,13 @@ import 'firebase/auth'
 import 'firebase/firebase-storage'
 
 const config = {
-  apiKey: process.env.NEXT_API_KEY,
-  authDomain: process.env.NEXT_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PROJECT_ID,
-  storageBucket: process.env.NEXT_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_APP_ID,
-  measurementId: process.env.NEXT_MEASUREMENT_ID,  
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,  
 }
 
 if (!firebase.apps.length) {
